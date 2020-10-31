8d7a0d4613ac43121acaee1f072b2a5dd48a70c9
Nathan Blaylock <ngblaylock@gmail.com>
2020-10-31 09:09:10 -0600

    Add Busy Bee Project

--- pages/projects/busy-bee-odd-jobs.vue
+++ pages/projects/busy-bee-odd-jobs.vue
@@ -1,71 +1,124 @@
 <template>
-  <div class="container-sm">
-    <h1>
-      Busy Bee Odd Jobs
-    </h1>
-    <p>
-      When Wayne approached me about his Busy Bee Odd Jobs request, I was
-      excited to take on a branding and website project for a budding small
-      business. There were a few good things going for his business. First thing
-      is that Wayne had a unique business name. Because of this, he didn’t have
-      any issues getting a good website domain; busybeeoddjobs.com. Another
-      thing he had going for him is that he started early in his business to get
-      his branding in order, and he had a good vision for what he wanted his
-      brand to be. First, Wayne needed was a logo. He sent over some clipart and
-      put words around it. He had a general idea for what he wanted his brand to
-      be but wasn’t able to execute it himself. I had drawn up a few sketches
-      for a logo and really liked the end result.
-    </p>
-    <img src="https://placehold.it/600x400" alt="" class="d-block mx-auto mb-3">
-    <p>
-      It is always fun to insert something subtle into a logo, similar to the
-      famous FedEx arrow. In this example, if you turn the logo sideways, you
-      can make out the word “ODD” as the main body of the bee. Although it is
-      not the full name of the business, it is part of it and helps tie in the
-      logo to the brand.
-    </p>
-    <img src="https://placehold.it/600x400" alt="" class="d-block mx-auto mb-3">
-    <p>
-      In the initial logo concept that Wayne provided, he had a somewhat rough
-      font. The typography chosen for Busy Bee Odd Jobs is Manslava, combined
-      with Varela Round. Both fonts are free and open-source so that helps Wayne
-      out financially. I liked how Manslava is a rough and somewhat grimy font.
-      Since Busy Bee Odd Jobs will do most any type of labor-intensive jobs, it
-      supports the fact that they are willing to do the messy and tough jobs.
-    </p>
-    <img src="https://placehold.it/600x400" alt="" class="d-block mx-auto mb-3">
-    <p>
-      Colors were pretty simple to choose for this project. Having a bee for a
-      logo, I played around mostly with yellow and black color schemes. I ended
-      up with a honey gradient and a dark brown color which turned out
-      fantastic. When you incorporate those colors with a honeycomb-hexagon
-      pattern, you get a solid, recognizable brand.
-    </p>
-    <img src="https://placehold.it/600x400" alt="" class="d-block mx-auto mb-3">
-    <p>
-      The website was a lot of fun to put together. Wayne didn’t need much on a
-      website, just enough to show what he can do and get people to contact him.
-      We decided to keep it at just a single page. Wayne and I brainstormed a
-      good hero statement “You have the jobs. We have the manpower.” We added a
-      call to action, which is to hire Wayne, and continued by sharing what
-      services he offered. We decided to have a little fun with showing some
-      projects that Wayne took on, and used a “Before and After” slider, so the
-      users can get an interactive experience. Of course, we needed a
-      testimonial and a contact form. To add a cherry on top of the website, we
-      added some animation to help improve the customer’s interest.
-    </p>
-    -- Busy Bee Home Page
-    <p>
-      It was a pleasure to work with Wayne and take on this Busy Bee Odd Jobs
-      branding and web development. I was even more pleased to hear about his
-      successes in increasing the jobs he saw come through.
-    </p>
+  <div>
+    <img src="/img/projects/bboj/BBOJ-hero.png" class="img-fluid" alt="" />
+    <div class="container-sm">
+      <h1>
+        Busy Bee Odd Jobs
+      </h1>
+      <p>
+        When Wayne approached me about his Busy Bee Odd Jobs request, I was
+        excited to take on a branding and website project for a budding small
+        business. There were a few good things going for his business. First
+        thing is that Wayne had a unique business name. Because of this, he
+        didn’t have any issues getting a good website domain;
+        busybeeoddjobs.com. Another thing he had going for him is that he
+        started early in his business to get his branding in order, and he had a
+        good vision for what he wanted his brand to be. First, Wayne needed was
+        a logo. He sent over some clipart and put words around it. He had a
+        general idea for what he wanted his brand to be but wasn’t able to
+        execute it himself. I had drawn up a few sketches for a logo and really
+        liked the end result.
+      </p>
+      <img
+        src="/img/projects/bboj/BBOJ-old-new.png"
+        alt=""
+        class="d-block mx-auto mb-3 img-fluid"
+      />
+      <p>
+        It is always fun to insert something subtle into a logo, similar to the
+        famous FedEx arrow. In this example, if you turn the logo sideways, you
+        can make out the word “ODD” as the main body of the bee. Although it is
+        not the full name of the business, it is part of it and helps tie in the
+        logo to the brand.
+      </p>
+      <img
+        src="/img/projects/bboj/BBOJ-odd.png"
+        alt=""
+        class="d-block mx-auto mb-3 img-fluid"
+      />
+      <p>
+        In the initial logo concept that Wayne provided, he had a somewhat rough
+        font. The typography chosen for Busy Bee Odd Jobs is Mansalva, combined
+        with Varela Round. Both fonts are free and open-source so that helps
+        Wayne out financially. I liked how Mansalva is a rough and somewhat
+        grimy font. Since Busy Bee Odd Jobs will do most any type of
+        labor-intensive jobs, it supports the fact that they are willing to do
+        the messy and tough jobs.
+      </p>
+      <img
+        src="/img/projects/bboj/BBOJ-fonts.png"
+        alt=""
+        class="d-block mx-auto mb-3 img-fluid"
+        style="width: 400px;"
+      />
+      <p>
+        Colors were pretty simple to choose for this project. Having a bee for a
+        logo, I played around mostly with yellow and black color schemes. I
+        ended up with a honey gradient and a dark brown color which turned out
+        fantastic. When you incorporate those colors with a honeycomb-hexagon
+        pattern, you get a solid, recognizable brand.
+      </p>
+      <img
+        src="/img/projects/bboj/BBOJ-colors.png"
+        alt=""
+        class="d-block mx-auto mb-3 img-fluid"
+      />
+      <p>
+        The website was a lot of fun to put together. Wayne didn’t need much on
+        a website, just enough to show what he can do and get people to contact
+        him. We decided to keep it at just a single page. Wayne and I
+        brainstormed a good hero statement “You have the jobs. We have the
+        manpower.” We added a call to action, which is to hire Wayne, and
+        continued by sharing what services he offered. We decided to have a
+        little fun with showing some projects that Wayne took on, and used a
+        “Before and After” slider, so the users can get an interactive
+        experience. Of course, we needed a testimonial and a contact form. To
+        add a cherry on top of the website, we added some animation to help
+        improve the customer’s interest.
+      </p>
+      <img
+        src="/img/projects/bboj/BBOJ-screenshot.png"
+        alt=""
+        class="d-block mx-auto mb-3 img-fluid"
+      />
+
+      <p class="text-center">
+        <a
+          href="/img/projects/bboj/BBOJ-website.png"
+          target="_blank"
+          class="btn btn-primary"
+          >Preview Website</a
+        >
+      </p>
+      <div class="alert alert-light">
+        Visit
+        <a href="https://busybeeoddjobs.com" target="_blank"
+          >busybeeoddjobs.com</a
+        >
+        to view the actual website. Please note, that some changes may have been
+        made by others who worked on the project after me, so not every aspect
+        can be attributed to my work.
+      </div>
+      <p>
+        It was a pleasure to work with Wayne and take on this Busy Bee Odd Jobs
+        branding and web development. I was even more pleased to hear about his
+        successes in increasing the jobs he saw come through.
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  head: {
+    title: "Busy Bee Odd Jobs Project",
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content: "My project for Busy Bee Odd Jobs was for a small business who needed a new logo and working website.",
+      },
+    ],
+  },
+};
 </script>
-
-<style>
-</style>
