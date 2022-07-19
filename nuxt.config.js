261d8624ec7f5b12b5736752ec05cc2793a030f5
Nathan Blaylock <ngblaylock@gmail.com>
2022-07-19 09:36:24 -0600

    Using Bootstrap Vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,10 @@
-import tailwindConfig from './tailwind.config'
-
 export default {
+  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Portfolio',
+    title: 'portfolio',
     htmlAttrs: {
       lang: 'en',
     },
@@ -11,28 +12,35 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'preconnect',
-        href: 'https://fonts.googleapis.com',
-      },
-      {
-        rel: 'preconnect',
-        href: 'https://fonts.gstatic.com',
-        crossorigin: '',
-      },
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap',
-      },
-    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '@/assets/scss/nathanblaylock.scss'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-  buildModules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
-  modules: ['@nuxt/content'],
-  tailwindcss: {
-    config: tailwindConfig,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+  bootstrapVue: {
+    bootstrapCSS: false, // Or `css: false`
+    // bootstrapVueCSS: false // Or `bvCSS: false`
   },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    Homepage
+  <div class="container text-primary">
+    <h1>Nathan Blaylock - Bootstrap</h1>
   </div>
 </template>
+
+<style lang="scss" scoped>
+
+</style>
