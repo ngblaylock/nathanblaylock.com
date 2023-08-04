c2bd102ef18a1fa1d861cbb8e7027bb2018f44f3
Nathan Blaylock <ngblaylock@gmail.com>
2023-08-03 22:46:33 -0600

    Copy lots of assets over

--- blunt.config.cjs
+++ blunt.config.cjs
@@ -4,13 +4,10 @@ module.exports = [
     output: "./static/images",
     sizes: [
       {
-        width: 100,
-        height: 100,
+        width: 1400,
         prefix: "thumb",
       },
     ],
     // See: https://sharp.pixelplumbing.com/api-resize
-    fit: "outside",
-    background: { r: 0, g: 0, b: 0, alpha: 1 },
   },
 ];
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,5 +1,33 @@
-<div class="container py-3 d-flex flex-wrap gap-3 justify-content-end">
-  <a href="/">Projects</a>
-  <a href="/about">About Nathan</a>
-  <a href="/contact">Contact</a>
-</div>
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+  let currentRoute: string;
+	const isCurrentRoute = (route:string) => {
+			currentRoute = route;
+	};
+  $: isCurrentRoute($page.route.id || '')
+</script>
+
+<nav>
+	<div class="container py-3 d-flex flex-wrap gap-3 justify-content-end">
+		<a href="/" class={currentRoute == '/' ? 'active' : ''}>Projects</a>
+		<a href="/about" class={currentRoute == '/about' ? 'active' : ''}>About Nathan</a>
+		<a href="/contact" class={currentRoute == '/contact' ? 'active' : ''}>Contact</a>
+	</div>
+</nav>
+
+<style lang="scss">
+	nav {
+		display: block;
+		font-size: 1.25rem;
+		font-weight: 600;
+		padding: 8px 12px;
+		a {
+			text-decoration: none;
+      border-bottom: 2px solid transparent;
+      &.active{
+        border-bottom: 2px solid $secondary;
+      }
+		}
+	}
+</style>
