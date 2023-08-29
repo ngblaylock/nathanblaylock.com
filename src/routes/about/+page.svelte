832e803ac797f5de3ba55653e469673831e74137
Nathan Blaylock <ngblaylock@gmail.com>
2023-08-28 21:38:11 -0600

    add privacy policy

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,9 +4,13 @@
 
 <SEO title="About" />
 
-<div class="row">
+<div class="row align-items-center">
 	<div class="col-sm-3">
-		<img src="https://via.placeholder.com/200" alt="Nathan Blaylock Portrait" />
+		<img
+			src="/images/optimized/profile/sm_blaylock-nathan.jpg"
+			alt="Nathan Blaylock Portrait"
+			class="rounded img-fluid"
+		/>
 	</div>
 	<div class="col-sm-9">
 		<p>
@@ -27,3 +31,107 @@
 		</p>
 	</div>
 </div>
+
+<div class="row mt-3 mt-md-5">
+	<div class="col-md-6">
+		<h3>Career</h3>
+		<div class="grouping">
+			<h4>Space Dynamics Laboratory</h4>
+			<ul>
+				<li>Developed and designed internal web applications and external websites.</li>
+				<li>
+					Implemented the Small Satellite Conference virtual conference website for 2020 and 2021
+					during the pandemic.
+				</li>
+				<li>UX/UI Lead</li>
+			</ul>
+			<h4 class="mt-4">Utah State University</h4>
+			<ul>
+				<li>
+					Lead multimedia efforts through graphic design, videography, branding, basic tech support,
+					and photography.
+				</li>
+				<li>Lead design and development on various department and program websites.</li>
+			</ul>
+			<h4 class="mt-4">Freelance</h4>
+			<ul>
+				<li>Designed and developed small to medium size websites for businesses.</li>
+				<li>Delivered quality branding and graphic design when needed.</li>
+			</ul>
+		</div>
+		<h3 class="mt-5">Education</h3>
+		<div class="grouping">
+			<h4>Utah State University</h4>
+			<ul>
+				<li>
+					<strong>Master of Arts</strong> - Instructional Technology and Learning Sciences. Area of study:
+					User Experience Design.
+				</li>
+				<li>
+					<strong>Bachelor of Science</strong> - Major in Art and Design, double minor in Computer Science
+					and Multimedia Development. Areas of study: Graphic Design, Web Development, and other multimedia
+					skills.
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="col-md-6 mt-5 mt-md-0 mb-5">
+		<h3>Tools &amp; Technology</h3>
+		<div class="grouping">
+			<h4>Proficient In</h4>
+			<ul>
+				<li>Figma, Adobe XD, Sketch</li>
+				<li>HTML, Markdown</li>
+				<li>CSS, SASS</li>
+				<li>Bootstrap 3, 4, 5, Bootstrap Vue, Vuetify</li>
+				<li>JS, jQuery, Vue, Nunjucks, Alpine</li>
+				<li>Eleventy, Nuxt, VuePress</li>
+				<li>Frontend JS Libraries (day.js, excel.js, chart.js, lodash, etc)</li>
+				<li>NPM</li>
+				<li>Git, GitHub, Bitbucket</li>
+				<li>Jira, Asana, Trello</li>
+				<li>Mac, Linux, Windows</li>
+				<li>Photoshop, Illustrator, InDesign</li>
+				<li>Visual Studio Code</li>
+			</ul>
+			<h4 class="mt-4">Dabbled In</h4>
+			<ul>
+				<li>Svelte, SvelteKit</li>
+				<li>Node.js, Express, PHP</li>
+				<li>TypeScript</li>
+				<li>Rollup</li>
+				<li>Strapi, Firebase, Postgres, MySQL</li>
+				<li>Bash, GitHub Actions</li>
+			</ul>
+			<h4 class="mt-4">Interested to Try</h4>
+			<ul>
+				<li>React, Angular</li>
+				<li>Gatsby, Astro</li>
+				<li>Vite, Snowpack</li>
+				<li>Electron, NodeGUI</li>
+				<li>MongoDB</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+	.grouping {
+  border-left: 1px solid $gray-300;
+  padding-left: 24px;
+  padding-top: 6px;
+  h4 {
+    margin-top: 0.25em;
+    &::before {
+      content: '';
+      position: absolute;
+      margin-top: 8px;
+      width: 16px;
+      height: 16px;
+      border-radius: 20px;
+      background: $primary;
+      margin-left: -32px;
+    }
+  }
+}
+</style>
