d69f6b8580d274aa45f3acb56b3e85504403d942
Nathan Blaylock <ngblaylock@gmail.com>
2024-07-04 14:06:08 -0600

    archive group sale tracker

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,78 +1,79 @@
 <script>
-	import SEO from '$components/SEO.svelte';
+  import SEO from '$components/SEO.svelte';
 
-	let projects = [
-		{
-			alt: 'Space Dynamics Laboratory',
-			route: 'sdl',
-		},
-		{
-			alt: 'Cookbook',
-			route: 'cookbook',
-		},
-		{
-			alt: 'Greater Smithfield Chamber of Commerce',
-			route: 'smithfield-chamber-of-commerce',
-		},
-		{
-			alt: 'Scoresheet App',
-			route: 'scoresheet',
-		},
-		{
-			alt: 'Extract GSheet',
-			route: 'extract-gsheet',
-		},
-		{
-			alt: 'Placeholdate Figma Plugin',
-			route: 'placeholdate',
-		},
-		{
-			alt: 'Small Satellite Conference',
-			route: 'smallsat',
-		},
-		{
-			alt: 'Group Sale Tracker',
-			route: 'group-sale-tracker',
-		},
-		{
-			alt: 'Jeffrey R. Wangsgard and Associates',
-			route: 'jrwcpatax',
-		},
-		{
-			alt: 'Busy Bee Odd Jobs',
-			route: 'busy-bee-odd-jobs',
-		},
-		{
-			alt: 'Bingo Caller',
-			route: 'bingo-caller',
-		},
-		{
-			alt: 'Sorenson Center',
-			route: 'scce',
-		},
-		// {
-		// 	alt: 'Psychology Department Website',
-		// 	route: 'psychology',
-		// },
-		// {
-		// 	alt: 'Enoch Initiative',
-		// 	route: 'enoch-initiative',
-		// },
-	];
+  let projects = [
+    {
+      alt: 'Space Dynamics Laboratory',
+      route: 'sdl',
+    },
+    {
+      alt: 'Cookbook',
+      route: 'cookbook',
+    },
+    {
+      alt: 'Greater Smithfield Chamber of Commerce',
+      route: 'smithfield-chamber-of-commerce',
+    },
+    {
+      alt: 'Scoresheet App',
+      route: 'scoresheet',
+    },
+    {
+      alt: 'Extract GSheet',
+      route: 'extract-gsheet',
+    },
+    {
+      alt: 'Placeholdate Figma Plugin',
+      route: 'placeholdate',
+    },
+    {
+      alt: 'Small Satellite Conference',
+      route: 'smallsat',
+    },
+    {
+      alt: 'Jeffrey R. Wangsgard and Associates',
+      route: 'jrwcpatax',
+    },
+    {
+      alt: 'Busy Bee Odd Jobs',
+      route: 'busy-bee-odd-jobs',
+    },
+    {
+      alt: 'Bingo Caller',
+      route: 'bingo-caller',
+    },
+    {
+      alt: 'Sorenson Center',
+      route: 'scce',
+    },
+    // // ARCHIVED PROJECTS
+    // {
+    // 	alt: 'Psychology Department Website',
+    // 	route: 'psychology',
+    // },
+    {
+      alt: 'Enoch Initiative',
+      route: 'enoch-initiative',
+    },
+    // {
+    // 	alt: 'Group Sale Tracker',
+    // 	route: 'group-sale-tracker',
+    // },
+  ];
 </script>
 
 <SEO title="Projects" robots />
 
 <div class="row mb-5">
-	{#each projects as project}
-		<div class="col-sm-4 mb-2">
-			<a href="/projects/{project.route}"
-				><img
-					src="/images/optimized/projects/{project.route}/lg_{project.route}.jpg"
-					alt={project.alt}
-					class="img-fluid rounded shadow"
-				/></a
-			>
-		</div>
-	{/each}
+  {#each projects as project}
+    <div class="col-sm-4 mb-2">
+      <a href="/projects/{project.route}"
+        ><img
+          src="/images/optimized/projects/{project.route}/lg_{project.route}.jpg"
+          alt={project.alt}
+          class="img-fluid rounded shadow"
+        /></a
+      >
+    </div>
+  {/each}
 </div>
