2fbcbeb9c5b9b9fc2e9bb64398c79395c7d13c25
Nathan Blaylock <ngblaylock@gmail.com>
2024-11-09 21:12:07 -0700

    Add 404 page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,1 +1,7 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+  import * as E from '$components/Elemental';
+</script>
+
+<h1>{$page.status} {$page.error?.message}</h1>
 <a href="/" class="btn btn-secondary">Home Page</a>
