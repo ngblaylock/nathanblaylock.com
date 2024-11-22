1ee01e056b0ad98f04e4623daf4408d42c01250e
Nathan Blaylock <ngblaylock@gmail.com>
2024-11-22 08:30:56 -0700

    Icon Buttons and Typography

---
+++ src/lib/components/Elemental/IconBtn.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { type IconName } from '$lib/icons';
+  import type { Snippet } from 'svelte';
+  import Icon from './Icon.svelte';
+  interface Props {
+    href?: string;
+    variant?: Variant;
+    class?: string;
+    icon: IconName;
+    outline?: boolean;
+    [key: string]: unknown;
+  }
+  let {
+    class: classList = '',
+    href = '',
+    variant = 'primary',
+    icon,
+    outline = false,
+    ...restProps
+  }: Props = $props();
+
+  const outlineVariant = $derived(outline ? 'outline-' : '');
+</script>
+
+{#if href}
+  <a class="btn btn-{outlineVariant}{variant} btn-icon {classList}" {href}><Icon name={icon} size={1.5} /></a>
+{:else}
+  <button class="btn btn-{outlineVariant}{variant} btn-icon {classList}" {...restProps}><Icon name={icon} size={1.5} /></button>
+{/if}
--- src/routes/uikit/components/+page.svelte
+++ src/routes/uikit/components/+page.svelte
@@ -21,6 +21,51 @@
           <E.Btn variant="gray">Button</E.Btn>
           <E.Btn variant="dark">Button</E.Btn>
         </div>
+        
+        <h3 class="mt-4">Inner Icon Buttons</h3>
+        <div>
+          <E.Btn variant="primary" iconLeft="arrowLeft">Button</E.Btn>
+          <E.Btn variant="primary" iconRight="arrowRight">Button</E.Btn>
+          <E.Btn variant="primary" iconLeft="arrowLeft" iconRight="arrowRight">Button</E.Btn>
+        </div>
+        
+        <h3 class="mt-4">Icon Buttons</h3>
+        <div>
+          <E.IconBtn variant="primary" icon="home">Button</E.IconBtn>
+          <E.IconBtn variant="secondary" icon="home">Button</E.IconBtn>
+          <E.IconBtn variant="dark" icon="home">Button</E.IconBtn>
+          <E.IconBtn variant="gray" icon="home">Button</E.IconBtn>
+          <E.IconBtn variant="light" icon="home">Button</E.IconBtn>
+          <E.IconBtn variant="base-1" icon="home">Button</E.IconBtn>
+          <E.IconBtn variant="base-i1" icon="home">Button</E.IconBtn>
+        </div>
+        <div>
+          <h4>Active</h4>
+          <E.IconBtn class="active" variant="primary" icon="home">Button</E.IconBtn>
+          <E.IconBtn class="active" variant="secondary" icon="home">Button</E.IconBtn>
+          <E.IconBtn class="active" variant="dark" icon="home">Button</E.IconBtn>
+          <E.IconBtn class="active" variant="gray" icon="home">Button</E.IconBtn>
+          <E.IconBtn class="active" variant="light" icon="home">Button</E.IconBtn>
+          <E.IconBtn class="active" variant="base-1" icon="home">Button</E.IconBtn>
+          <E.IconBtn class="active" variant="base-i1" icon="home">Button</E.IconBtn>
+        </div>
+        <div>
+          <h4>Disabled</h4>
+          <E.IconBtn disabled variant="primary" icon="home">Button</E.IconBtn>
+          <E.IconBtn disabled variant="secondary" icon="home">Button</E.IconBtn>
+          <E.IconBtn disabled variant="dark" icon="home">Button</E.IconBtn>
+          <E.IconBtn disabled variant="gray" icon="home">Button</E.IconBtn>
+          <E.IconBtn disabled variant="light" icon="home">Button</E.IconBtn>
+          <E.IconBtn disabled variant="base-1" icon="home">Button</E.IconBtn>
+          <E.IconBtn disabled variant="base-i1" icon="home">Button</E.IconBtn>
+        </div>
+        
+        <h3 class="mt-4">Outline Icon Buttons</h3>
+        <div>
+          <E.IconBtn variant="primary" outline icon="home">Button</E.IconBtn>
+          <E.Btn variant="primary" outline>Button</E.Btn>
+          <E.Btn variant="primary" outline iconRight="arrowRight" iconLeft="arrowLeft">Button</E.Btn>
+        </div>
       </div>
     </div>
   </div>
