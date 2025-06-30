d6af36e7c624009bdfcdbbd27b512f5ff6d1b10e
Nathan Blaylock <ngblaylock@gmail.com>
2025-06-30 07:32:56 -0600

    update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import { defineConfig } from 'vite';
 import Components from 'unplugin-svelte-components/vite';
 
 export default defineConfig({
-  plugins: [
-    Components({
-      dirs: ['src/PACKAGE/components'],
-      dts: true,
-      eslintrc: {
-        enabled: false,
-      }
-    }),
-    sveltekit(),
-  ],
+	plugins: [
+		Components({
+			dirs: ['src/PACKAGE/components'],
+			dts: true,
+			eslintrc: {
+				enabled: false
+			}
+		}),
+		sveltekit()
+	]
 });
